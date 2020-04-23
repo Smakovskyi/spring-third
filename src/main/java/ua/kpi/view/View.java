2fe06d28ca893fd9efb358f607ec3536a18bd3d3
d.smakovskiy <smakovskiy@ukr.net>
2020-04-23 09:59:49 +0300

    One more change

--- src/main/java/ua/kpi/view/View.java
+++ src/main/java/ua/kpi/view/View.java
@@ -1,14 +1,20 @@
 package ua.kpi.view;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:config.properties")
 @Scope(scopeName = "prototype")
 public class View {
 
+  @Value("${viewable.filler}")
+  private String filler;
+
   public View() {
-    System.out.println("===============View instance is created============");
+    System.out.println(filler + "View instance is created" + filler);
   }
 
   public void print(String value) {
